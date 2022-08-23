6baae623a3eb5b13ff5d4e55c7eba69105e30e54
David Cheng <32114983+ChengDave@users.noreply.github.com>
2022-08-23 03:28:24 -0600

    implemented basic chat app with django channels

--- chatproject/chatproject/wsgi.py
+++ chatproject/chatproject/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for davedock project.
+WSGI config for chat project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'davedock.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatproject.settings')
 
 application = get_wsgi_application()
